e16e1a1c7fa3ce1ff26f3b4c789b65b78993f826
geo.c <geo.c@daumkakao.com>
2015-05-20 14:21:10 +0900

    이런것이 message-driven 방식 아닐까?

--- src/main/java/geo/Controller.java
+++ src/main/java/geo/Controller.java
@@ -4,7 +4,6 @@ import geo.service.KeyStore;
 import geo.service.Repository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +12,7 @@ import org.springframework.web.context.request.async.DeferredResult;
 import rx.Observable;
 import rx.Subscriber;
 import rx.schedulers.Schedulers;
+import rx.subjects.AsyncSubject;
 
 @RestController
 @Slf4j
@@ -102,6 +102,54 @@ public class Controller {
 				});
 			}
 		});
+
+		log.info("servlet end.");
+		return deferredResult;
+	}
+
+	@RequestMapping("/messageDriven/{query}")
+	public DeferredResult<String> getByMessage(@PathVariable String query) {
+		DeferredResult deferredResult = new DeferredResult();
+		AsyncSubject<String> querySubject = AsyncSubject.create();
+		AsyncSubject<String> keySubject = AsyncSubject.create();
+		AsyncSubject<String> resultSubject = AsyncSubject.create();
+
+		querySubject.subscribe(query1 -> {
+			keyStore.getKeyLater2(query1).addCallback(new ListenableFutureCallback<String>() {
+				@Override
+				public void onSuccess(String key) {
+					keySubject.onNext(key);
+					keySubject.onCompleted();
+				}
+
+				@Override
+				public void onFailure(Throwable ex) {
+					deferredResult.setResult("matching key not found.");
+				}
+			});
+		});
+
+		keySubject.subscribe(key -> {
+			repository.findLater2(key).addCallback(new ListenableFutureCallback<String>() {
+				@Override
+				public void onSuccess(String result) {
+					resultSubject.onNext(result);
+					resultSubject.onCompleted();
+				}
+
+				@Override
+				public void onFailure(Throwable ex) {
+					deferredResult.setResult("");
+				}
+			});
+		});
+
+		resultSubject.subscribe(result -> {
+			deferredResult.setResult(result);
+		});
+
+		querySubject.onNext(query);
+		querySubject.onCompleted();
 
 		log.info("servlet end.");
 		return deferredResult;
