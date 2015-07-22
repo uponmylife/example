cda890edf7c6155b460f7d6c69ea4f09d416913f
geo.c <geo.c@daumkakao.com>
2015-07-22 10:33:26 +0900

    clear tracing code

--- src/main/java/geo/UserController.java
+++ src/main/java/geo/UserController.java
@@ -1,13 +1,10 @@
 package geo;
 
-import kamon.trace.Tracer;
-import kamon.util.Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,16 +14,6 @@ public class UserController {
 
 	@RequestMapping("/users")
 	public List<UserDetail> getUsers() {
-		return Tracer.withNewContext("trace2", true, (Supplier<List<UserDetail>>) () -> {
-			Supplier<Set<String>> getUserIds = () -> repository.getUserIds();
-			Set<String> userIds = Tracer.currentContext().withNewSegment("getUserIds", "database", "jdbc", getUserIds);
-
-			List<UserDetail> userDetails = userIds.stream().map((id) -> {
-				Supplier<UserDetail> getDetail = () -> repository.getUserDetail(id);
-				return Tracer.currentContext().withNewSegment("getDetail", "database", "jdbc", getDetail);
-			}).collect(Collectors.toList());
-
-			return userDetails;
-		});
+		return repository.getUserIds().stream().map((id) -> repository.getUserDetail(id)).collect(Collectors.toList());
 	}
 }
